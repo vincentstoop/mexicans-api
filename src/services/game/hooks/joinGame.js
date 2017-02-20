daf87e7eb525476ab5b7d4f114ccdc88dca4aede
vincentstoop <vincentstoop@gmail.com>
2017-02-20 16:54:48 +0100

    joinGame hook

---
+++ src/services/game/hooks/joinGame.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// const errors = require('feathers-errors');
+
+module.exports = function(options) {
+  return function(hook) {
+    return hook.app.service('games').get(hook.id)
+      .then((game) => {
+        // console.log(game.players);
+        if (hook.params.user._id !== game.gameMasterId) {
+          const userIsPlayer = game.players.some((player) => {
+            console.log('player', player);
+            return player.userId.toString() === hook.params.user._id.toString() 
+          })
+          if (!userIsPlayer) {
+            const newPlayer = { userId: hook.params.user._id, userName: hook.params.user.name };
+            hook.data.players = [].concat(game.players, newPlayer);
+          }
+        }
+      })
+  }
+}
